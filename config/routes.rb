3f03f7b849c64760131fd7d9ad348020ae78adef
Nina Shirole <ninashirole@uchicago.edu>
2021-07-28 22:37:24 +0000

    88.9

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   get("/", { :controller => "application", :action => "homepage" })
   get("/directors", { :controller => "directors", :action => "index" })
+  get("/movies", { :controller => "movies", :action => "index" })
+  get("/actors", { :controller => "actors", :action => "index" })
   get("/directors/eldest", { :controller => "directors", :action => "eldest" })
   get("/directors/youngest", { :controller => "directors", :action => "youngest" })
 #flexible route goes after static routes
   get("/directors/:an_id", { :controller => "directors", :action => "details" })
+  get("/movies/:an_id", { :controller => "movies", :action => "details" })
+  get("/actors/:an_id", { :controller => "actors", :action => "details" })
 end
