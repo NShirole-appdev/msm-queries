256bc40fe4f6439bf6d30f2ec19c7f083a277ffc
Nina Shirole <ninashirole@uchicago.edu>
2021-07-28 04:41:16 +0000

    youngest and oldest

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -6,7 +6,12 @@ class DirectorsController < ApplicationController
   end
 
   def eldest
-    
+    @eldest_director = Director.order({ :dob => :asc }).where.not({ :dob =>  nil }).at(0)
     render({ :template => "director_templates/eldest.html.erb"})
+  end
+
+  def youngest
+    @youngest_director = Director.order({ :dob => :desc }).where.not({ :dob =>  nil }).at(0)
+    render({ :template => "director_templates/youngest.html.erb"})
   end
 end
